dd7520e92fd5a70841e3ea1d917f585426d7f35a
zk524 <zk524gg@gmail.com>
2023-11-05 17:41:53 +0800

    read && write

--- index.js
+++ index.js
@@ -1,21 +1,42 @@
+
+
 window.addEventListener('NEOLine.N3.EVENT.READY', () => {
+    console.log('ready')
     const neolineN3 = new NEOLineN3.Init();
-    document.getElementById("exec").addEventListener("keyup", async function (event) {
+    document.getElementById("read").onclick = async () => {
         try {
-            if (event.code !== "Enter") return;
-            const url = new URL(event.target.value);
+            const { value: url } = document.getElementById("exec")
+            console.log(url)
             if (url.protocol !== "n3:") throw 'protocol must be n3!';
             const [_0, _1, sender, _3, scopes, scripthash, method] = url.pathname.match(new RegExp('^//(([a-zA-Z0-9]+)(:(.*))?@)?([a-zA-Z0-9]+)/([a-zA-Z0-9]+)$'));
             const ret = await neolineN3.invokeRead({
                 scriptHash: scripthash,
                 operation: method,
                 args: [...url.searchParams].map(([k, v]) => ({ type: k, value: v })),
-                signers: [{account: sender ?? (await neolineN3.getAccount()).address, scopes: scopes?parseInt(scopes):1}],
+                signers: [{ account: sender ?? (await neolineN3.getAccount()).address, scopes: scopes ? parseInt(scopes) : 1 }],
             });
             alert(JSON.stringify(ret));
         } catch (err) {
             console.log(err);
             alert(err);
         }
-    });
+    }
+    document.getElementById("write").onclick = async () => {
+        try {
+            const { value: url } = document.getElementById("exec")
+            console.log(url)
+            if (url.protocol !== "n3:") throw 'protocol must be n3!';
+            const [_0, _1, sender, _3, scopes, scripthash, method] = url.pathname.match(new RegExp('^//(([a-zA-Z0-9]+)(:(.*))?@)?([a-zA-Z0-9]+)/([a-zA-Z0-9]+)$'));
+            const ret = await neolineN3.invokde({
+                scriptHash: scripthash,
+                operation: method,
+                args: [...url.searchParams].map(([k, v]) => ({ type: k, value: v })),
+                signers: [{ account: sender ?? (await neolineN3.getAccount()).address, scopes: scopes ? parseInt(scopes) : 1 }],
+            });
+            alert(JSON.stringify(ret));
+        } catch (err) {
+            console.log(err);
+            alert(err);
+        }
+    }
 });
